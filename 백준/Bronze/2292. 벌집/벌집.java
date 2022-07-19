14cd7e5a5c8cbe2ed661baca7679a393745ebc0d
윤규빈 <100677209+Gyu-Bin-Yoon@users.noreply.github.com>
2022-07-19 16:31:07 +0900

    [Bronze II] Title: 벌집, Time: 128 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,26 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n =Integer.parseInt(br.readLine());
+
+        int check =1;
+        int i=1;
+        while(n>check){
+            check += 6*i;
+            i++;
+        }
+
+        bw.write(String.valueOf(i));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
